199a84e85776ee6a473ced2780434e18f0345887
Josué Florián <150297452+JosueFlorian17@users.noreply.github.com>
2024-07-02 14:49:39 -0500

    Add files via upload

--- SRC/Ejercicio6-1.py
+++ SRC/Ejercicio6-1.py
@@ -1,15 +1,15 @@
 import boto3
 from botocore.exceptions import ClientError
 
-# Crear clientes de DynamoDB en diferentes regiones
-dynamodb_us_west_2 = boto3.client('dynamodb', region_name='us-west-2')
-dynamodb_us_east_1 = boto3.client('dynamodb', region_name='us-east-1')
 
-# Nombre de la tabla
-table_name = 'TablapruebaGlobal'
+dynamodb_us_west_2=boto3.client('dynamodb', region_name='us-west-2')
+dynamodb_us_east_1=boto3.client('dynamodb', region_name='us-east-1')
 
-# Definición de la tabla
-table_definition = {
+
+table_name='TablapruebaGlobal'
+
+
+table_definition={
     'TableName': table_name,
     'KeySchema': [
         {
@@ -31,36 +31,28 @@ table_definition = {
             'AttributeType': 'S'
         }
     ],
-    'ProvisionedThroughput': {
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
+    'BillingMode': 'PAY_PER_REQUEST'  # Usamos un modelo Pay-Per-Request para poder crear tablas adicionales en diferentes regiones 
 }
 
 try:
-    # Crear tabla en us-west-2
     response_us_west_2 = dynamodb_us_west_2.create_table(**table_definition)
     print(f"Tabla creada en us-west-2: {response_us_west_2}")
 
-    # Crear tabla en us-east-1
-    response_us_east_1 = dynamodb_us_east_1.create_table(**table_definition)
-    print(f"Tabla creada en us-east-1: {response_us_east_1}")
-
-    # Esperar a que ambas tablas estén activas
     dynamodb_us_west_2.get_waiter('table_exists').wait(TableName=table_name)
-    dynamodb_us_east_1.get_waiter('table_exists').wait(TableName=table_name)
 
-    # Asociar la tabla como tabla global
-    dynamodb_us_west_2.update_table(
+    # Replicamos la tabla en la región nel Norte de Virginia
+    response_add_replica = dynamodb_us_west_2.update_table(
         TableName=table_name,
-        ReplicationGroup=[
+        ReplicaUpdates=[
             {
-                'RegionName': 'us-east-1'
+                'Create':{
+                    'RegionName':'us-east-1'
+                }
             }
         ]
     )
 
-    print(f"Tabla global '{table_name}' creada exitosamente.")
+    print(f"Réplica añadida en us-east-1: {response_add_replica}")
 
 except ClientError as e:
     print(f"Error al crear la tabla global: {e}")
