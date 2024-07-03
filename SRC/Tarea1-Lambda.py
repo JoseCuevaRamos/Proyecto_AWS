74ab0db256e9694839df6f847344c8f3ccc7ffe0
Josué Florián <150297452+JosueFlorian17@users.noreply.github.com>
2024-07-02 22:38:54 -0500

    Update Tarea1-Lambda.py

--- SRC/Tarea1-Lambda.py
+++ SRC/Tarea1-Lambda.py
@@ -3,38 +3,18 @@ from botocore.exceptions import ClientError
 
 # Crear un cliente para DynamoDB
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-table = dynamodb.Table('prueba')
+table = dynamodb.Table('pruebafinalaws')
 
 try:
     # Añadir un nuevo elemento para generar un evento
     response = table.put_item(
         Item={
             'Id': '2',     # Clave de partición
-            'Datte': '2024-07-02T00:00:00Z',  # Clave de clasificación, usando formato de fecha como ejemplo
+            'Date': '2024-01-01',  # Clave de clasificación, usando formato de fecha como ejemplo
             'OtherAttribute': 'Valor de prueba2'  # Otros atributos adicionales
         }
     )
     print("Elemento añadido:", response)
-
-    # Actualizar el elemento para generar otro evento
-    response = table.update_item(
-        Key={
-            'Id': '2',     # Clave de partición
-            'Datte': '2024-07-02T00:00:00Z'  # Clave de clasificación
-        },
-        UpdateExpression='SET OtherAttribute = :val1',
-        ExpressionAttributeValues={':val1': 'Nuevo valor'}
-    )
-    print("Elemento actualizado:", response)
-
-    # Eliminar el elemento para generar otro evento
-    response = table.delete_item(
-        Key={
-            'Id': '2',     # Clave de partición
-            'Datte': '2024-07-02T00:00:00Z'  # Clave de clasificación
-        }
-    )
-    print("Elemento eliminado:", response)
 
 except ClientError as e:
     print(f"Error al manipular la tabla: {e}")
